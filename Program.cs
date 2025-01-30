b4284267d6debe69ecf67f17302e179ca610b615
olemartin25sfj <olemartin25fj@gmail.com>
2025-01-30 11:00:58 +0100

    base

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return "Get from '/'";
+});
+
+app.MapPost("/", () =>
+{
+    return "Posting to '/'";
+});
+
+app.MapPut("/", () =>
+{
+    return "Updating '/'";
+});
+
+app.MapDelete("/", () =>
+{
+    return "Deleting '/'";
+});
 
 app.Run();
